6f2a1555acb5b1390453eba33d60d55d484b958c
Khalil Chatoo <chatoor@khalil-mbp.cbc-rc.ca>
2017-12-05 12:06:40 -0500

    Added unit tests for ExpenseListFilters component

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -9,11 +9,21 @@ export class ExpenseListFilters extends React.Component {
 	};
 
 	onDatesChange = ({ startDate, endDate }) => {
-		this.props.dispatch(setStartDate(startDate));
-		this.props.dispatch(setEndDate(endDate));
+		this.props.setStartDate(startDate);
+		this.props.setEndDate(endDate);
 	};
 	onFocusChange = (calendarFocused) => {
 		this.setState(() => ({ calendarFocused }))
+	};
+	onTextChange = (e) => {
+		this.props.setTextFilter(e.target.value);
+	};
+	onSortChange = (e) => {
+		if (e.target.value === 'date') {
+			this.props.sortByDate();
+		} else {
+			this.props.sortByAmount();
+		}
 	};
 	render () {
 		return (
@@ -21,19 +31,11 @@ export class ExpenseListFilters extends React.Component {
 				<input
 					type="text"
 					value={this.props.filters.text}
-					onChange={(e) => {
-						this.props.dispatch(setTextFilter(e.target.value));
-					}}
+					onChange={this.onTextChange}
 				/>
 				<select
 					value={this.props.filters.sortBy}
-					onChange={(e) => {
-						if (e.target.value === 'date') {
-							this.props.dispatch(sortByDate());
-						} else {
-							this.props.dispatch(sortByAmount());
-						}
-				}}>
+					onChange={this.onSortChange}>
 					<option value="date">Date</option>
 					<option value="amount">Amount</option>
 				</select>
@@ -52,10 +54,16 @@ export class ExpenseListFilters extends React.Component {
 	}
 }
 
-const mapStateToProps = (state) => {
-	return {
-		filters: state.filters
-	};
-};
+const mapStateToProps = (state) => ({
+	filters: state.filters
+});
 
-export default connect(mapStateToProps)(ExpenseListFilters);
+const mapDispatchToProps = (dispatch) => ({
+	setTextFilter: (text) => dispatch(setTextFilter(text)),
+	sortByDate: () => dispatch(sortByDate),
+	sortByAmount: () => dispatch(sortByAmount),
+	setStartDate: (startDate) => dispatch(setStartDate(startDate)),
+	setEndDate: (endDate) => dispatch(setEndDate)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -1,3 +1,68 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { ExpenseListFilters } from '../../components/ExpenseListFilters';
+import { filters, altFilters } from '../fixtures/filters';
+import { DateRangePicker } from 'react-dates';
+
+let setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate, wrapper;
+
+beforeEach(() => {
+	setTextFilter = jest.fn();
+	sortByDate = jest.fn();
+	sortByAmount = jest.fn();
+	setStartDate = jest.fn();
+	setEndDate = jest.fn();
+	wrapper = shallow(
+		<ExpenseListFilters 
+			filters={filters}
+			setTextFilter={setTextFilter}
+			sortByDate={sortByDate}
+			sortByAmount={sortByAmount}
+			setStartDate={setStartDate}
+			setEndDate={setEndDate}
+		/>
+	);
+});
+
+test('should render ExpenseListFilters correctly', () => {
+	expect(wrapper).toMatchSnapshot();
+});
+
+test('should render ExpenseListFilters with altFilters correctly', () => {
+	wrapper.setProps({ filters: altFilters });
+	expect(wrapper).toMatchSnapshot();
+});
+
+//should handle text change
+test('should handle test change', () => {
+	wrapper.find('input').simulate('change', { target: { value: '10' }});
+	expect(setTextFilter).toHaveBeenLastCalledWith('10');
+});
+
+//should handle sort by date
+test('should handle sort by date', () => {
+	wrapper.find('select').simulate('change', { target: { value: 'date' }});
+	expect(sortByDate).toHaveBeenCalled();
+});
+
+//should handle sort by amount
+test('should handle sort by amount', () => {
+	wrapper.find('select').simulate('change', { target: { value: 'amount' }});
+	expect(sortByAmount).toHaveBeenCalled();
+});
+
+//should handle date changes
+test('should handle date changes', () => {	
+	wrapper.find(DateRangePicker).prop('onDatesChange')({
+		startDate: 0,
+		endDate: 1
+	});
+	expect(setStartDate).toHaveBeenLastCalledWith(0);
+	expect(setEndDate).toHaveBeenLastCalledWith(1);
+});
+
+//should handle date focus changes
+test('should handle date focus changes', () => {
+	wrapper.find(DateRangePicker).prop('onFocusChange')('endDate');
+	expect(wrapper.state('calendarFocused')).toBe('endDate');
+});
